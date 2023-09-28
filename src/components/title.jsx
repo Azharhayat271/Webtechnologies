c54b7bc4c33e11f860884874ed11954e1d9807d1
Azharhayat271 <115901212+Azharhayat271@users.noreply.github.com>
2023-09-28 08:30:18 -0700

    check commit

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -5,7 +5,7 @@ function CustomTitle() {
     <div>
    <div className='text-4xl lg:text-[2.6rem] md:font-extrabold lg:leading-[2.9rem] font-bold text-white leading-10'>
    <span className='text-green-400 mx-2'>Web</span>
-   <span className='text-pink-500 mx-2'>Technologies</span>
+   <span className='text-pink-500 mx-2'>Technologiess</span>
    <br></br>
 
   
