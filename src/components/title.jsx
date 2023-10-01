c1b38d3ce5c236a7424b3e0d9f53a2c18aeadfe3
Azharhayat271 <115901212+Azharhayat271@users.noreply.github.com>
2023-10-01 06:15:07 -0700

    animation added to the title

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 function CustomTitle() {
+  const textVariants = {
+    initial: {
+      opacity: 0,
+      y: 20,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
     <div>
-   <div className='text-4xl lg:text-[2.6rem] md:font-extrabold lg:leading-[2.9rem] font-bold text-white leading-10'>
-   <span className='text-green-400 mx-2'>Web</span>
-   <span className='text-pink-500 mx-2'>Technologies</span>
-   <br></br>
-
-  
-
-   </div>
-    
+      <div className='text-4xl lg:text-[2.6rem] md:font-extrabold lg:leading-[2.9rem] font-bold text-white leading-10'>
+        <motion.span
+          className='text-green-400 mx-2'
+          variants={textVariants}
+          initial='initial'
+          animate='animate'
+        >
+          Web
+        </motion.span>
+        <motion.span
+          className='text-pink-500 mx-2'
+          variants={textVariants}
+          initial='initial'
+          animate='animate'
+        >
+          Technologies
+        </motion.span>
+        <br></br>
+      </div>
     </div>
   );
 }
